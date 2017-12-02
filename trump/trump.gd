72e3eb1d13b2dc724c00247ab6779ab711b2ebb3
TheDerek <mail@derek.tech>
2017-12-02 14:15:35 +0000

    Added tweet

--- trump/trump.gd
+++ trump/trump.gd
@@ -15,35 +15,35 @@ func _ready():
 
 func _process(delta):
 	var direction = 0;
-	
+
 	if Input.is_action_pressed("run_right"):
 		direction += 1;
-	
+
 	if Input.is_action_pressed("run_left"):
 		direction -=1;
-	
+
 	if direction == 1:
 		apply_impulse(Vector2(), Vector2(speed * delta, 0))
 		if animation.get_current_animation() != 'run_right':
 			animation.set_current_animation('run_right')
-			
+
 		if not animation.is_playing():
 			animation.play()
-		
+
 	if direction == -1:
 		apply_impulse(Vector2(), Vector2(-speed * delta, 0))
 		if animation.get_current_animation() != 'run_left':
 			animation.set_current_animation('run_left')
-			
+
 		if not animation.is_playing():
 			animation.play()
-			
+
 	if direction == 0:
 		if last_direction == 1:
 			animation.set_current_animation('stand_right')
-		
+
 		if last_direction == -1:
 			animation.set_current_animation('stand_left')
-	
+
 	if direction != 0:
 		last_direction = direction
